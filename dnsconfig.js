fd2d54eebf63817b3fe0ebfca77b39e145135671
Takashi Takizawa <taki@cyber.email.ne.jp>
2024-06-16 21:30:17 +0900

    pilsnerの削除

--- dnsconfig.js
+++ dnsconfig.js
@@ -3,6 +3,6 @@ var DSP_SAKURACLOUD = NewDnsProvider("sakuracloud");
 
 D("dnsbeer.com", REG_NONE, DnsProvider(DSP_SAKURACLOUD),
   DefaultTTL(3600),
-  A("pale-ale", "192.0.2.1", TTL(1800)),
-  A("pilsner", "192.0.2.2"),
+  HTTPS("@", 1, "pale-ale.dnsbeer.com"),
+  A("pale-ale", "192.0.2.1"),
 END);
