24553f5085d6794dfc45c0c9a6cda3b21391c5cb
Takashi Takizawa <taki@cyber.email.ne.jp>
2024-06-16 18:25:22 +0900

    fix

--- dnsconfig.js
+++ dnsconfig.js
@@ -4,5 +4,5 @@ var DSP_SAKURACLOUD = NewDnsProvider("sakuracloud");
 D("dnsbeer.com", REG_NONE, DnsProvider(DSP_SAKURACLOUD),
   DefaultTTL(3600),
   A("pale-ale", "192.0.2.1"),
-  A("pilsner", "192.0.2.3"
+  A("pilsner", "192.0.2.2"),
 END);
