63af94fd6949199102d287f8dd5162f6ced6136e
Takashi Takizawa <taki@cyber.email.ne.jp>
2024-06-16 21:49:29 +0900

    HTTPSレコードの誤りの修正

--- dnsconfig.js
+++ dnsconfig.js
@@ -3,6 +3,6 @@ var DSP_SAKURACLOUD = NewDnsProvider("sakuracloud");
 
 D("dnsbeer.com", REG_NONE, DnsProvider(DSP_SAKURACLOUD),
   DefaultTTL(3600),
-  HTTPS("@", 1, "pale-ale.dnsbeer.com"),
+  HTTPS("@", 1, "pale-ale.dnsbeer.com", ""),
   A("pale-ale", "192.0.2.1"),
 END);
